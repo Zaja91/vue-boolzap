e1bb4253c1a8226cb4344fad5d7b8a65f5ed7bc2
Zaja91 <yagos.terni@gmail.com>
2020-11-22 12:49:31 +0100

    improving

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const vm = new Vue({
   data: {
     userName: "Andrei",
     userImg: "img/avatar_1.jpg",
-    filterFriends: "",
+    searchText: "",
     // Un array con tutti i msg inviati dal utente
     myMessagges: [],
     friendsList: [
@@ -28,11 +28,44 @@ const vm = new Vue({
         friendMessagges: [],
       },
     ],
+    filteredList: [
+      {
+        friendName: "Michele",
+        friendImg: "img/avatar_2.jpg",
+        friendMessagges: [],
+      },
+      {
+        friendName: "Fabio",
+        friendImg: "img/avatar_3.jpg",
+        friendMessagges: [],
+      },
+      {
+        friendName: "Samuele",
+        friendImg: "img/avatar_4.jpg",
+        friendMessagges: [],
+      },
+      {
+        friendName: "Luisa",
+        friendImg: "img/avatar_5.jpg",
+        friendMessagges: [],
+      },
+    ],
+    selectedFriend: {},
   },
   methods: {
     filteredFriend: function () {
-      this.friendsList = this.friendsList.filter(e => e.friendName.toLowerCase().startsWith(this.filterFriends));
+      if (this.searchText) {
+        this.filteredList = this.friendsList.filter((e) =>
+          e.friendName.toLowerCase().startsWith(this.searchText.toLowerCase())
+        );
+      } else {
+        this.filteredList = this.friendsList;
+      }
+    },
+    showInfo: function (friend) {
+      return friend;
     },
   },
   computed: {},
+  watch: {},
 });
