b5c827546c02da5f8aabfa66fc0279ca9cdc13eb
Zaja91 <yagos.terni@gmail.com>
2020-11-23 12:39:29 +0100

    milestone 2

--- js/main.js
+++ js/main.js
@@ -3,51 +3,83 @@ const vm = new Vue({
   data: {
     userName: "Andrei",
     userImg: "img/avatar_1.jpg",
+    userMessage: '',
     searchText: "",
+    possibleAnswers: ["ok", "ciao e buona serata", "non posso ora"],
     // Un array con tutti i msg inviati dal utente
     myMessagges: [],
-    friendsList: [
-      {
+    friendsList: [{
         friendName: "Michele",
         friendImg: "img/avatar_2.jpg",
-        friendMessagges: [],
+        friendMessagges: [{
+            name: 'Michele',
+            message: "Hello motherfucker"
+          },
+          {
+            name: 'Michele',
+            message: "Hello motherfucker"
+          },
+          {
+            name: 'Michele',
+            message: "Hello motherfucker"
+          },
+          {
+            name: 'Andrei',
+            message: "Hello motherfucker"
+          },
+        ],
       },
       {
         friendName: "Fabio",
         friendImg: "img/avatar_3.jpg",
-        friendMessagges: [],
+        friendMessagges: [{}],
       },
       {
         friendName: "Samuele",
         friendImg: "img/avatar_4.jpg",
-        friendMessagges: [],
+        friendMessagges: [{}],
       },
       {
         friendName: "Luisa",
         friendImg: "img/avatar_5.jpg",
-        friendMessagges: [],
+        friendMessagges: [{}],
       },
     ],
-    filteredList: [
-      {
+    filteredList: [{
         friendName: "Michele",
         friendImg: "img/avatar_2.jpg",
-        friendMessagges: [],
+        friendMessagges: [{
+            name: 'Michele',
+            message: "Hello motherfucker"
+          },
+          {
+            name: 'Michele',
+            message: "Hello motherfucker"
+          },
+          {
+            name: 'Michele',
+            message: "Hello motherfucker"
+          },
+          {
+            name: 'Andrei',
+            message: "Hello motherfucker im your worst",
+          }
+        ],
       },
       {
         friendName: "Fabio",
         friendImg: "img/avatar_3.jpg",
-        friendMessagges: [],
+        friendMessagges: [{}],
       },
       {
         friendName: "Samuele",
         friendImg: "img/avatar_4.jpg",
-        friendMessagges: [],
+        friendMessagges: [{}],
       },
       {
         friendName: "Luisa",
         friendImg: "img/avatar_5.jpg",
-        friendMessagges: [],
+        friendMessagges: [{}],
       },
     ],
     selectedFriend: {},
@@ -65,6 +97,9 @@ const vm = new Vue({
     showInfo: function (friend) {
       return friend;
     },
+    checkUsername: function (name) {
+      return name == this.userName
+    }
   },
   computed: {},
   watch: {},
