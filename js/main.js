361aa05e023bf4e74b0da7105cc96962ce724ff9
Zaja91 <yagos.terni@gmail.com>
2020-11-24 12:04:41 +0100

    milestone 4 + some css bugs to fix

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+// Rules
+// 1) Small functions
+// 2) Blocks within functions should be 1 line long and call a function gennerally
+// 3) What does it return
+// 4) Functions that are used in other functions have to be declared on top
+// 5) Functions should only do 1 thing without side-effects
+// 6) Do not overlap naming var and functions
+// 7) Specific name functions(preffer longer clearer naming that shorter fuzzy ones)
+// 8) Do not abbreviate names
+
+// First draft always messy refine it until it reads almost like a logical step by step
+
 const vm = new Vue({
   el: "#root",
   data: {
@@ -7,12 +19,10 @@ const vm = new Vue({
     searchText: "",
     possibleAnswers: ["ok", "ciao e buona serata", "non posso ora"],
     // Un array con tutti i msg inviati dal utente
-    myMessagges: [],
     friendsList: [{
         friendName: "Michele",
         friendImg: "img/avatar_2.jpg",
-        friendMessagges: [{}
-        ],
+        friendMessagges: [{}],
       },
       {
         friendName: "Fabio",
@@ -30,42 +40,41 @@ const vm = new Vue({
         friendMessagges: [{}],
       },
     ],
-    filteredList: [{
-        friendName: "Michele",
-        friendImg: "img/avatar_2.jpg",
-        friendMessagges: [{}
-        ],
-      },
-      {
-        friendName: "Fabio",
-        friendImg: "img/avatar_3.jpg",
-        friendMessagges: [{}],
-      },
-      {
-        friendName: "Samuele",
-        friendImg: "img/avatar_4.jpg",
-        friendMessagges: [{}],
-      },
-      {
-        friendName: "Luisa",
-        friendImg: "img/avatar_5.jpg",
-        friendMessagges: [{}],
-      },
-    ],
+    // filteredList: [{
+    //     friendName: "Michele",
+    //     friendImg: "img/avatar_2.jpg",
+    //     friendMessagges: [{}],
+    //   },
+    //   {
+    //     friendName: "Fabio",
+    //     friendImg: "img/avatar_3.jpg",
+    //     friendMessagges: [{}],
+    //   },
+    //   {
+    //     friendName: "Samuele",
+    //     friendImg: "img/avatar_4.jpg",
+    //     friendMessagges: [{}],
+    //   },
+    //   {
+    //     friendName: "Luisa",
+    //     friendImg: "img/avatar_5.jpg",
+    //     friendMessagges: [{}],
+    //   },
+    // ],
     selectedFriend: {},
   },
   methods: {
     // I needed to create a separate array and update based on filter input and
     // then display it without changing the original list of friends
-    filteredFriend: function () {
-      if (this.searchText) {
-        this.filteredList = this.friendsList.filter((e) =>
-          e.friendName.toLowerCase().startsWith(this.searchText.toLowerCase())
-        );
-      } else {
-        this.filteredList = this.friendsList;
-      }
-    },
+    // filteredFriend: function () {
+    //   if (this.searchText) {
+    //     this.filteredList = this.friendsList.filter((e) =>
+    //       e.friendName.toLowerCase().startsWith(this.searchText.toLowerCase())
+    //     );
+    //   } else {
+    //     this.filteredList = this.friendsList;
+    //   }
+    // },
     showInfo: function (friend) {
       return friend;
     },
@@ -82,14 +91,23 @@ const vm = new Vue({
     randomMsg: function () {
       return this.possibleAnswers[Math.floor(Math.random() * this.possibleAnswers.length)]
     },
+    generateAnswer: function(activeFriend) {
+      return activeFriend.friendMessagges.push({
+        name: activeFriend.friendName,
+        message: this.randomMsg()
+      })
+    },
     friendAnswer: function (activeFriend) {
-      setTimeout(
-        (activeFriend.friendMessagges.push({
-          name: activeFriend.friendName,
-          message: this.randomMsg()
-        }), 5000)
-
-      )
+      setTimeout(() => {this.generateAnswer(activeFriend)},3000)
+    }
+  },
+  computed: { 
+      // This gives me an error of (handler.apply is not a function) but allows me to dont 
+      // duplicate the friendsList 
+    filteredFriend: function () {
+      return this.friendsList.filter((e) =>
+        e.friendName.toLowerCase().startsWith(this.searchText.toLowerCase())
+      );
     }
   },
 });
